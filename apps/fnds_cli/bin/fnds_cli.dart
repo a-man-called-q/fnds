e1ccca23c0fe45d8010924103fbfd5c6b9f0e552
Muhammad Tsany Qudsi <phlacx@hotmail.com>
2025-03-27 00:15:55 +0700

    🎉 Begin the adventure
    Starting new melos template

---
+++ apps/fnds_cli/bin/fnds_cli.dart
@@ -0,0 +1,5 @@
+import 'package:fnds_cli/fnds_cli.dart' as fnds_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fnds_cli.calculate()}!');
+}
---
+++ apps/fnds_cli/test/fnds_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:fnds_cli/fnds_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
